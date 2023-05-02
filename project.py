67c0b20da2b319e90f25704dd53621981feda9b4
NSC1337 <71302512+NSC1337@users.noreply.github.com>
2023-05-02 14:10:39 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,15 +1,17 @@
 import random
 
-def monteCarlo(f, trials):
+# Estimates probability of success of experiment().
+def monteCarlo(experiment, trials):
     sum = 0
     for i in range(trials):
-        sum += f()
+        sum += experiment()
     return sum / trials
 
-def birthdayTest(sample):
+# Returns whether or not people share a birthday in a random sample of size n.
+def birthdayTest(n):
     def foo():
         A = set([])
-        for i in range(sample):
+        for i in range(n):
             x = random.randint(1,365)
             if x in A:
                 return True
@@ -18,7 +20,8 @@ def birthdayTest(sample):
         return False
     return foo
 
-
+# Creates a list of probabilities using monte carlo experiments
+# ex: data[23] = P(a group of 23 people will have shared bdays)
 data = list(map(lambda y : monteCarlo(birthdayTest(y), 100), range(1,30)))
 
 print(data)
